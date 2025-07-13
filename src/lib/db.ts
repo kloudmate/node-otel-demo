40f66bafd0f5d665f55fb97611508fb640d2ff59
Pratham Sahu <prathamsahu051@gmail.com>
2025-07-13 22:04:09 +0530

    removed mongodb credentails

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -3,7 +3,7 @@ import { logger } from '../logger';
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI as string || 'mongodb+srv://prathamsahu051:pratham051@cluster0.g2s42sz.mongodb.net/node-otel?retryWrites=true&w=majority');
+    await mongoose.connect(process.env.MONGO_URI as string);
     logger.info('MongoDB connected successfully');
   } catch (error) {
     logger.info('MongoDB connection error:', error);
