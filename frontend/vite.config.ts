dac3961425e103e4fc8d56aa9a0fa1038ba1525e
Pratham Sahu <prathamsahu051@gmail.com>
2025-07-15 11:41:02 +0530

    removed env

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,7 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), 'VITE_');
-
-  return {
-    define: {
-      'process.env': {
-        OTEL_PROPAGATORS: JSON.stringify(env.VITE_OTEL_PROPAGATORS || 'tracecontext,baggage'),
-      }
-    }
-  };
-});
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
